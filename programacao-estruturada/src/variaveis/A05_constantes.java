50ad9bf6ffc4b753b2c02d845a74310daab08e63
osvaldo souza <osvaldo.ark@gmail.com>
2024-06-19 19:18:12 -0300

    feat - adicionando novas constantes

--- programacao-estruturada/src/variaveis/A05_constantes.java
+++ programacao-estruturada/src/variaveis/A05_constantes.java
@@ -11,7 +11,11 @@ public class A05_constantes {
         final double TAXA_ANUAL_DE_FINANCIAMENTO = scan.nextDouble();
 
         //minha mudança. 
-        final double PI = 3;
+        final double PI = 3.14;
+
+        final double AVOGRADO = 6.02 * Math.pow(10,23);
+
+        final double GRAVIDADE_NA_TERRA = 9.8;
 
         scan.close();
 

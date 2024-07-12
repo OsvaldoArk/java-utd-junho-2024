6f03ce33d804d8c9e8d8a170e261d11bcc39b968
Nicolas Cide <nicolasextreme780@gmail.com>
2024-07-12 15:44:49 -0300

    feat - adicionando tarefa nicolas cide

---
+++ programacao-estruturada/tarefa-de-casa/hackaton java iniciante/nicolasteste.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class nicolasteste {
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+        int randnumero = (int) ((Math.random() *100 ) + 1);
+        int palpite = 0;
+        System.out.println("Olá,esse é o jogo do Advinha,você possui 10 tentativas para advinhar um número aleatório entre 1 e 100. ");
+
+        for(int cont = 0;cont <= 10;cont++){    
+            if (cont > 9){
+                System.out.println("Nenhuma tentativa restante");
+                break;
+                
+            }
+            System.out.println("Digite seu palpite:");
+            palpite = leitor.nextInt();
+            if (cont < 9) {
+                
+            
+                if (palpite > randnumero) {
+                System.out.println("Tente Novamente.O número que você procura é menor que "+palpite+" Você possui: "+ (10 - (cont+1))+" tentativas.");      
+            }
+            else if (palpite < randnumero) {
+                System.out.println("Tente Novamente.O número que você procura é maior que "+palpite+" Você possui: "+ (10 - (cont+1))+" tentativas.");
+            }
+            else{
+                System.out.println("Parabéns! Você advinhou o número sorteado que é: " +randnumero);
+                break;        
+            }
+        }
+     }
+        leitor.close();
+    }
+}
